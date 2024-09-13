abe4f08ae097bdc3cec08505ff04a2668e086925
Lulu <112640408+HopeICanChangeMyUsername@users.noreply.github.com>
2024-09-13 05:03:59 -0300

    Implement basic linked list

---
+++ linked_list.js
@@ -0,0 +1,41 @@
+export default class LinkedList {
+  head = null;
+
+  append(key, value) {
+    if (this.head === null) {
+      const newNode = new Node(key, value);
+      this.head = newNode;
+      return;
+    }
+    let currentNode = this.head;
+    while (currentNode.nextNode !== null) {
+      currentNode = currentNode.nextNode;
+    }
+    //nextNode === null
+    const newNode = new Node(key, value);
+    currentNode.nextNode = newNode;
+  }
+
+  toString() {
+    let str = "Head -> ";
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      str += `(key: ${currentNode.key}, value:${currentNode.value}) -> `;
+      currentNode = currentNode.nextNode;
+    }
+    str += `null`;
+    return str;
+  }
+}
+
+export class Node {
+  constructor(key, value, nextNode = null) {
+    this.key = key;
+    this.value = value;
+    this.nextNode = nextNode;
+  }
+
+  setNext(nextNode) {
+    this.nextNode = nextNode;
+  }
+}
--- main.js
+++ main.js
@@ -1,10 +1,22 @@
 import HashMap from "./hash_maps.js";
+import LinkedList from "./linked_list.js";
 console.log("hi");
-const hm = new HashMap();
+// const hm = new HashMap();
 
-hm.hash("banano");
-hm.hash("Banano");
-hm.hash("ananoB");
-hm.hash("cat");
-hm.hash("a");
-hm.hash("b");
+// hm.hash("banano");
+// hm.hash("Banano");
+// hm.hash("ananoB");
+// hm.hash("cat");
+// hm.hash("a");
+// hm.hash("b");
+
+const lList = new LinkedList();
+console.log(lList.toString());
+
+console.log(lList);
+lList.append("age", "13");
+
+lList.append("age", "15");
+lList.append("age", "17");
+console.log(lList);
+console.log(lList.toString());
